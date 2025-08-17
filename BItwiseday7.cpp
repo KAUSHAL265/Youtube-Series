7be1ba527e91fabc49b3ba2b613f3cfe58ed81e1
My Name <someone@gmail.com>
2025-08-17 23:09:53 +0530

    Always Learn

---
+++ BItwiseday7.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    // int a =4, b =8, c = 10;
+
+    // cout<< (a & b)<<endl;
+    // cout<< (a | b)<<endl;
+    // cout<<(a ^ b)<<endl;
+    // cout<<(a << 1)<<endl;
+    // cout<<(c << 2)<<endl;
+    // cout<<(c >> 1)<<endl;
+    // cout<<(b >> 2)<<endl;
+    // cout<<(6 & 10)<<endl;
+    // cout<<(6 | 10)<<endl;
+    // cout<<(6 ^ 10)<<endl;
+    // cout<<(10 << 2)<<endl;
+    // cout<<(10 >> 1)<<endl;
+    // cout<<(5-2*6)<<endl;
+    // cout<<((5-2) * 6)<<endl;
+    // cout<<(4 * 5%2)<<endl;
+     cout<< sizeof(int) <<endl;
+     cout<< sizeof(long int) <<endl;
+     cout<< sizeof(short int) <<endl;
+     cout<< sizeof(long long int) <<endl;
+     cout<< sizeof( signed int)<<endl;
+     cout<< sizeof ( unsigned int)<< endl;
+
+     unsigned int x =-10;
+     cout<< x << endl;
+    return 0;
+}
